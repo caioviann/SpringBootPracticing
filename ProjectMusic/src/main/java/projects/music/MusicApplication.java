c483c4584f49ff94018914b6a4357f265c8cafce
caio henrique <caiohenriqueviana01@gmail.com>
2026-07-01 17:37:07 -0300

    add new projects

---
+++ ProjectMusic/src/main/java/projects/music/MusicApplication.java
@@ -0,0 +1,17 @@
+package projects.music;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApplication implements CommandLineRunner {
+	public static void main(String[] args){
+		SpringApplication.run(MusicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ ProjectMusic/src/main/java/projects/music/models/Artista.java
@@ -0,0 +1,4 @@
+package projects.music.models;
+
+public class Artista {
+}
