66d61086f08adc33bf0a47f76a54f858a2ab0279
caio henrique <caiohenriqueviana01@gmail.com>
2026-09-18 11:18:43 -0300

    variacoes de GET quer podem ser feitos

--- spring-boot-essentials/src/main/java/br/com/caio/spring_boot_essentials/controller/HelloWorldController.java
+++ spring-boot-essentials/src/main/java/br/com/caio/spring_boot_essentials/controller/HelloWorldController.java
@@ -2,26 +2,29 @@ package br.com.caio.spring_boot_essentials.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/hello")
 public class HelloWorldController {
 //    @GetMapping
-//    @ResponseStatus(HttpStatus.OK)
-//    public String helloWorld(){
-//        return "Hello World";
-//    }
-
-//    @GetMapping
 //    public ResponseEntity<String> helloWorld(){
 //        return ResponseEntity.ok("Hello World");
 //    }
 
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String get1(@PathVariable String id){
+        return "Hello World" + id;
+    }
+
     @GetMapping
-    public ResponseEntity<String> helloWorld(){
-        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    public ResponseEntity<String> get2(@RequestParam(value = "name", required = true) String name){
+        return new ResponseEntity<>("Hello World" + name, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> post(@RequestBody String name){
+        return new ResponseEntity<>("Hello world POST TESTE " + name, HttpStatus.CREATED);
     }
 }
