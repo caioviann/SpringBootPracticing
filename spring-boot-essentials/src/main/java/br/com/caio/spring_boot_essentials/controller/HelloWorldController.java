f431855f9df5c5b19aae4cab8d763e1ce6d225b1
caio henrique <caiohenriqueviana01@gmail.com>
2026-09-18 10:47:03 -0300

    variacoes de GET quer podem ser feitos

--- spring-boot-essentials/src/main/java/br/com/caio/spring_boot_essentials/controller/HelloWorldController.java
+++ spring-boot-essentials/src/main/java/br/com/caio/spring_boot_essentials/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package br.com.caio.spring_boot_essentials.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/hello")
 public class HelloWorldController {
+//    @GetMapping
+//    @ResponseStatus(HttpStatus.OK)
+//    public String helloWorld(){
+//        return "Hello World";
+//    }
+
+//    @GetMapping
+//    public ResponseEntity<String> helloWorld(){
+//        return ResponseEntity.ok("Hello World");
+//    }
+
     @GetMapping
-    public String helloWorld(){
-        return "Hello World";
+    public ResponseEntity<String> helloWorld(){
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
     }
 }
