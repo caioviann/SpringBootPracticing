dd8ec9a4aebd6346d1c9fb93179e5ecfbc06bb81
caio henrique <caiohenriqueviana01@gmail.com>
2026-09-24 15:26:45 -0300

    add new att

---
+++ spring-boot-essential-jpa/src/main/java/br/com/caio/spring_boot_essentials/database/repository/ITreinosRepository.java
@@ -0,0 +1,7 @@
+package br.com.caio.spring_boot_essentials.database.repository;
+
+import br.com.caio.spring_boot_essentials.database.model.TreinosEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITreinosRepository extends JpaRepository<TreinosEntity, Integer> {
+}
