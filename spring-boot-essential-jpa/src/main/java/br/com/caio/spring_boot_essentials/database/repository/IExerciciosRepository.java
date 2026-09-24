557ef94490d20dd3a1d4bb34eb01e16bb550b013
caio henrique <caiohenriqueviana01@gmail.com>
2026-09-24 15:32:13 -0300

    add JPQL

--- spring-boot-essential-jpa/src/main/java/br/com/caio/spring_boot_essentials/database/repository/IExerciciosRepository.java
+++ spring-boot-essential-jpa/src/main/java/br/com/caio/spring_boot_essentials/database/repository/IExerciciosRepository.java
@@ -2,6 +2,8 @@ package br.com.caio.spring_boot_essentials.database.repository;
 
 import br.com.caio.spring_boot_essentials.database.model.ExerciciosEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +11,11 @@ import java.util.List;
 public interface IExerciciosRepository extends JpaRepository<ExerciciosEntity, Integer> {
 
     List<ExerciciosEntity> findAllByGrupoMuscular(String grupoMuscular);
+
+    @Query(value = """
+        SELECT e
+        FROM Exercicios e 
+        WHERE UPPER(e.grupoMuscular) = UPPER(:grupoMuscular)
+    """)
+    List<ExerciciosEntity> findAllByGrupoMuscularJpql(@Param("grupoMuscular") String grupoMuscular);
 }
